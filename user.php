ba667cff934215d238f68333fcdbe48b5aa82334
Ethan Benckwitz <60863350+ebenckwitz@users.noreply.github.com>
2021-11-07 14:43:10 -0700

    Create user.php

---
+++ user.php
@@ -0,0 +1,10 @@
+<?php 
+$info = "ebenckwitz";
+echo "this is user.php. This the current users info: $info";
+
+
+
+ ?>
+<form action="main.php" method="get">
+  <input type="submit" value="Sign Out">
+</form>
