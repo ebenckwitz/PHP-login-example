a7df719a6fadc7e1583b384ca8a5a7152e54d8ac
Ethan Benckwitz <60863350+ebenckwitz@users.noreply.github.com>
2021-11-10 09:18:57 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <?php // login.php
-$host = "cssrvlab01.utep.edu";// Change as necessary
-$data = "db"; // Change as necessary
-$user = "user"; // Change as necessary
-$pass = "pass"; // Change as necessary
-$chrs = "utf8mb4";
-$attr = "mysql:host=$host;dbname=$data;charset=$chrs";
-$opts =
-[
-PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-PDO::ATTR_EMULATE_PREPARES => false,
-];
+ $host = "cssrvlab01.utep.edu";  
+ $username = "user"; //change as needed 
+ $password = "pass"; //change as needed  
+ $database = "db";  //change as needed
+
+ try  {  
+ 
+      $connect = new PDO("mysql:host=$host; dbname=$database", $username, $password);  
+      $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+ } catch(PDOException $error) {  
+      $msg = $error->getmessage();  
+ }
 ?>
