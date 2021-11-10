3b87103c81801116aabacbd1007eafcaa43e4395
Ethan Benckwitz <60863350+ebenckwitz@users.noreply.github.com>
2021-11-10 09:20:22 -0700

    Update user.php to display users information.

--- user.php
+++ user.php
@@ -1,10 +1,31 @@
-<?php 
-$info = "ebenckwitz";
-echo "this is user.php. This the current users info: $info";
+<?php
+session_start();
+echo "this is user.php. <br>";
 
+//Collect $user array from signin.php. Print out users first and last name with username as well.
+if (isset($_SESSION["user"]))
+{
+    $user = $_SESSION["username"];
+    $first = $_SESSION["firstname"];
+    $last = $_SESSION["lastname"];
+    echo "username: $user <br> first name: $first <br> last name: $last";
 
+    echo '<form action="logout.php" method="get">
+		  <input type="submit" value="Sign Out">
+		  </form>  ';
+}
+else
+{
+    header("location:signin.php");
+}
 
- ?>
-<form action="main.php" method="get">
-  <input type="submit" value="Sign Out">
+?>
+ 
+ <!DOCTYPE html>
+<html>
+    <head>
+<form action="logged_in.php" method="get">
+  <input type="submit" value="Main (logged in)">
 </form>
+</head>
+</html>
