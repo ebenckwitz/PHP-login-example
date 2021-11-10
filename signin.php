3ad59a411f6b31d3a5dc9ea110072843c6d53dc0
Ethan Benckwitz <60863350+ebenckwitz@users.noreply.github.com>
2021-11-10 09:19:37 -0700

    Update signin.php to work with database correctly.

--- signin.php
+++ signin.php
@@ -1,71 +1,63 @@
-<?php  
- session_start();  
- $host = "cssrvlab01.utep.edu";  
- $username = "ebenckwitz";  
- $password = "ethaniscool";  
- $database = "ebenckwitz_f21_db";  
- $message = ""; 
+<?php
+echo "Sign In page";
+require_once 'login.php';
+session_start();
 
- try  
- {  
-      $connect = new PDO("mysql:host=$host; dbname=$database", $username, $password);  
-      $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
-      if(isset($_POST["login"]))  
-      {  
-           if(empty($_POST["username"]) || empty($_POST["password"]))  
-           {  
-                $message = '<label>All fields are required</label>';  
-           }  
-           else  
-           {  
-                $query = "SELECT * FROM admin_users WHERE username = :username AND password = :password";  
-                $statement = $connect->prepare($query);  
-                $statement->execute(  
-                     array(  
-                          'username'     =>     $_POST["username"],  
-                          'password'     =>     $_POST["password"]  
-                     )  
-                );  
-                $count = $statement->rowCount();  
-                if($count > 0)  
-                {  
-                     $_SESSION["username"] = $_POST["username"];  
-                     header("location:main.php");  
-                }  
-                else  
-                {  
-                     $message = '<label>Incorrect Username and/or Password. Try Again.</label>';  
-                }  
-           }  
-      }  
- }  
- catch(PDOException $error)  
- {  
-      $message = $error->getMessage();  
- }  
- ?>  
+$msg = "";
+
+if (isset($_POST["login"]))
+{
+	//check if username and password fields are empty, if not, move on.
+    if (empty($_POST["username"]) || empty($_POST["password"]))
+    {
+        $msg = '<label>All fields are required</label>';
+    }
+    else
+    {
+		//Create sql query. Call it for the DB. Execute then make the array a $user. Then check if passwords match or not.
+        $query = "SELECT * FROM admin_users WHERE username = :username";
+        $statement = $connect->prepare($query);
+        $statement->execute(array(
+            'username' => $_POST["username"],
+        ));
+        $user = $statement->fetchAll();
+        $_SESSION["user"] = $user;
+
+        if (password_verify(str_replace("'", "", $_POST["password"]) , $user[0]['password']))
+        {
+            $_SESSION["username"] = $_POST["username"];
+            $_SESSION["password"] = $_POST["password"];
+            $_SESSION["firstname"] = $user[0]['firstname'];
+            $_SESSION["lastname"] = $user[0]['lastname'];
+            header("location:logged_in.php");
+        }
+        else
+        {
+            $msg = '<h2>Wrong Username and/or Password. Try Again.</h2>';
+        }
+    }
+}
+
+?>  
  <!DOCTYPE html>  
  <html>  
       <head>  
-           <title>Sign In Page</title>    
       </head>  
       <body>    
-           <div class="container" style="width:500px;">  
-                <?php  
-                if(isset($message))  
-                {  
-                     echo '<label class="text-danger">'.$message.'</label>';  
-                }  
-                ?>  
+           <div>  
+                <?php
+if (isset($msg))
+{
+    echo "$msg";
+} ?>  
                 <form class="form" action="" method="post">
-                    <h1 class="login-title">Sign In</h1>
                      <label>Username</label>  
-                     <input type="text" name="username" class="form-control" placeholder="Username" />  
+                     <input type="text" name="username" class="form-control" />  
                      <br />  
                      <label>Password</label>  
-                     <input type="password" name="password" class="form-control"  placeholder="Password"/>  
+                     <input type="password" name="password" class="form-control" />  
                      <br />  
-                     <input type="submit" name="login" class="btn btn-info" value="Sign In" />  <p class="link"><a href="registration.php">Click to Sign Up</a></p>
+                     <input type="submit" name="login" class="btn btn-info" value="Sign In" />
                 </form>  
            </div>  
       </body>  
